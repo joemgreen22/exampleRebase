b53b70020de46401177caf12da94f762f61866c6
joemgreen22 <52195596+joemgreen22@users.noreply.github.com>
2021-12-10 01:44:43 -0700

    This a pull request resolves issue #1  (#3)

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main
     public void methodInMaster()
     {
         System.out.println("Revision 1 in master");
+        System.out.println("first commit");
+        System.out.println("second commit");
+        System.out.println("third commit");
 
     }
 
